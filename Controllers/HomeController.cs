3709d41fd7b6b22c56923808783e3a92aa1c85c0
Dadi Zhou <david_melbourne@msn.com>
2018-02-12 18:18:58 +1100

    Minor clean up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,18 +19,6 @@ namespace CargoCult.Controllers
 
         public ActionResult Index()
         {
-            //List<Location> locations = new List<Location>
-            //{
-            //    new Location {
-            //        Name = "Melbourne CBD",
-            //        Position = GeographyPoint.Create(-37.813611,144.963056)
-            //    },
-            //    new Location {
-            //        Name = "Flagstaff Gardens",
-            //        Position = GeographyPoint.Create(-37.8111635,144.9544162)
-            //    }
-            //};
-            //ViewBag.Locations = locations;
             var latitude = TempData["Latitude"];
             var longitude = TempData["Longitude"];
             var searchRadius = TempData["SearchRadius"];
